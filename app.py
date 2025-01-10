c7fff13dd4ed39d31bb4027dadbf0097e25bf4bc
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2025-01-10 01:34:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from datetime import datetime
-from threading import Thread
 from flask import Flask
 import pickle
 import time
